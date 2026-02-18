fa4862121d8eb585b4c30dd692edd2027d543215
Mizanur Rahman <mdmizanurrahman5215@gmail.com>
2026-02-18 20:25:36 +0600

    update add money functionality

--- script/cashIn.js
+++ script/cashIn.js
@@ -1,45 +1,48 @@
- console.log("cash in connected");
+console.log("cash in connected");
 
- document.getElementById("cashin-btn").addEventListener("click", function(){
-    // get bank 
- const selectedBank = document.getElementById("select-bank")
- const bank  = selectedBank.value;
- const accountNumber = document.getElementById("account-number")
- const account  = accountNumber.value;
- const cashinAmount = document.getElementById("cashIn-amount")
- const amount  = cashinAmount.value;
- const accountPin = document.getElementById("account-pin")
- const pin  = accountPin.value;
- const balanceElem = document.getElementById("balance")
- let balance = balanceElem.innerText
- console.log(balance);
- 
+document.getElementById("addmoney-btn").addEventListener("click", function () {
+  // get bank
 
- if(amount < 1){
-    console.log("invalid ammount");
-    
- return;
- }
+  const bank = getInputFromId("select-bank");
+  console.log(bank);
+  if (bank === "") {
+    alert("Select a bank");
 
- if(  (bank === "BRAC" || bank === "DUTCH" || bank === "CITY") && account == "01234567890" && Number(amount) > 0 && pin == "1234"){
-     
-      let newBalance = Number(balance) + Number(amount);
-      balanceElem.innerText = newBalance
-      console.log(newBalance);
-      
-       console.log("cash in successfully");
-      
-     
-      
- }
- else{
-    
-    console.log("invalid input");
-    
+    return;
+  }
+  const bankAccount = getInputFromId("account-number");
+  if (bankAccount.length !== 11) {
+    alert("invalid account number");
+    return;
+  }
+
+  console.log(bankAccount);
+
+  const ammount = getInputFromId("addmoney-amount")
+  if(ammount < 1){
+    alert("Invalid ammount")
     return
- }
- 
 
-    
- })
- 
+  }
+  const pin = getInputFromId("addmoney-pin")
+   if (pin.length !== 4) {
+  alert("invalid pin number");
+    return;
+  }
+   const balance = getBalanced()
+   console.log(balance);
+
+ if( bank && bankAccount == "01234567890" && ammount && pin =="1234" ){
+    const newBalance = balance + Number(ammount)
+    updateBalance(newBalance)
+    alert("balanced added successfully")
+
+ }else{
+    alert("Something Wrong")
+    return
+}
+
+   
+
+
+});
---
+++ script/common.js
@@ -0,0 +1,28 @@
+console.log("common connected");
+
+
+
+const getInputFromId = (id) => {
+  const getResult = document.getElementById(id);
+  const result = getResult.value;
+
+  return result;
+};
+
+const getBalanced = () => {
+  const balanceElem = document.getElementById("balance");
+  const balance = balanceElem.innerText;
+  return Number(balance);
+};
+
+const updateBalance = (newBalance)=>{
+  const balanceElem = document.getElementById("balance");
+ balanceElem.innerText = newBalance;
+}
+const showOnly =(id)=>{
+  const getElem = document.getElementById(id);
+  console.log(getElem);
+  
+  getElem.classList.remove("hidden")
+}
+
